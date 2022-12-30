b2590fe2495a5d3ac7d979133b76b7061e7dd6bc
ec277 <reneevrardoc@hotmail.com>
2022-12-30 17:55:16 -0600

    Agregando elementos

--- app.js
+++ app.js
@@ -1,80 +1,105 @@
 
 
-function encriptarTexto(){
-    const texto = document.getElementById("input_texto").value.toLowerCase();
+/* función para encriptar texto */
+function encryptText(){
+    const text = document.getElementById("input_text").value.toLowerCase();
 
-    if (texto == "") { 
+    if (text == "") { 
   
         alert(" Ingresa texto para encriptar");
     
     } else {
-        let txtCifrado = texto.replace(/e/igm, "enter");
-         txtCifrado = texto.replace(/é/igm, "enter");
-         txtCifrado = txtCifrado.replace(/o/igm, "ober");
-         txtCifrado = txtCifrado.replace(/ó/igm, "ober");
-         txtCifrado = txtCifrado.replace(/i/igm, "imes");
-         txtCifrado = txtCifrado.replace(/í/igm, "imes");
-         txtCifrado = txtCifrado.replace(/a/igm, "ai");
-         txtCifrado = txtCifrado.replace(/á/igm, "ai");
-         txtCifrado = txtCifrado.replace(/u/igm, "ufat");
-         txtCifrado = txtCifrado.replace(/ú/igm, "ufat");
+        let encryptedText = text.replace(/e/igm, "enter");
+        encryptedText = text.replace(/é/igm, "enter");
+        encryptedText = encryptedText.replace(/o/igm, "ober");
+        encryptedText = encryptedText.replace(/ó/igm, "ober");
+        encryptedText = encryptedText.replace(/i/igm, "imes");
+        encryptedText = encryptedText.replace(/í/igm, "imes");
+        encryptedText = encryptedText.replace(/a/igm, "ai");
+        encryptedText = encryptedText.replace(/á/igm, "ai");
+        encryptedText = encryptedText.replace(/u/igm, "ufat");
+        encryptedText = encryptedText.replace(/ú/igm, "ufat");
 
         document.getElementById("img").style.display = "none";
-       // document.getElementById("texto").style.display = "none";
+       
        document.getElementById("texto2").style.display = "show";
        document.getElementById("texto2").style.display = "inherit";
-        document.getElementById("texto2").innerHTML = txtCifrado;
-       document.getElementById("copiar").style.display = "show";
-       document.getElementById("copiar").style.display = "inherit";
+       document.getElementById("texto2").innerHTML = encryptedText;
+       document.getElementById("copy").style.display = "show";
+       document.getElementById("copy").style.display = "inherit";
+       document.getElementById("nuevo").style.display = "show";
+       document.getElementById("nuevo").style.display = "inherit";
+       
     }
 
 }
 
-const btnEncriptar = document.getElementById("btn-encriptar");
-btnEncriptar.addEventListener("click", encriptarTexto);
+const btnEncrypt = document.getElementById("btn-encrypt");
+btnEncrypt.addEventListener("click", encryptText);
 
-function desencriptarTexto(){
-    const texto = document.getElementById("input_texto").value.toLowerCase();
+/* función para desencriptar texto */
+function decryptText(){
+    const text = document.getElementById("input_text").value.toLowerCase();
 
-    if (texto == "") { 
+    if (text == "") { 
   
         alert("Ingresa texto para desencriptar");
     
     } else {
 
-        let txtCifrado = texto.replace(/enter/igm, "e");
-        txtCifrado = texto.replace(/enter/igm, "é");
-         txtCifrado = txtCifrado.replace(/ober/igm, "o");
-         txtCifrado = txtCifrado.replace(/imes/igm, "i");
-         txtCifrado = txtCifrado.replace(/ai/igm, "a");
-         txtCifrado = txtCifrado.replace(/ufat/igm, "u");
-         txtCifrado = txtCifrado.replace(/ufat/igm, "ú");
+        let decryptedText = text.replace(/enter/igm, "e");
+        decryptedText = text.replace(/enter/igm, "é");
+        decryptedText = decryptedText.replace(/ober/igm, "o");
+        decryptedText = decryptedText.replace(/imes/igm, "i");
+        decryptedText = decryptedText.replace(/ai/igm, "a");
+        decryptedText = decryptedText.replace(/ufat/igm, "u");
+        decryptedText = decryptedText.replace(/ufat/igm, "ú");
 
-       // document.getElementById("imgDer").style.display = "none";
-       // document.getElementById("texto").style.display = "none";
-        document.getElementById("texto2").innerHTML = txtCifrado;
-       // document.getElementById("copiar").style.display = "show";
-       // document.getElementById("copiar").style.display = "inherit";
+      
+        document.getElementById("texto2").innerHTML = decryptedText;
+        
 
     }
 }
 
-const btnDesencriptar = document.getElementById("btn-desencriptar");
-btnDesencriptar.addEventListener("click", desencriptarTexto);
+const btnDecrypt = document.getElementById("btn-decrypt");
+btnDecrypt.addEventListener("click", decryptText);
 
-function copiar(){
-    const contenido = document.querySelector("#texto2");
-    const texto = document.getElementById("input_texto");
-    contenido.select();
+/* función para copiar texto */
+function copyText(){
+    const content = document.querySelector("#texto2");
+    const text = document.getElementById("input_text");
+    content.select();
     
-   // navigator.clipboard.writeText(contenido.value);
-    document.execCommand("copy");
+   navigator.clipboard.writeText(content.value);
+   // document.execCommand("copy");
     alert("Texto copiado");
 
-    texto.value = "";
+    text.value = "";
     
 
 }
 
-const btnCopiar = document.getElementById("copiar");
-btnCopiar.addEventListener("click", copiar);
+const btnCopy = document.getElementById("copy");
+btnCopy.addEventListener("click", copyText);
+
+
+/* vaciar input */
+
+ const inputText = document.getElementById('input_text');
+  inputText.addEventListener('click', function() {
+    this.value = '';
+  });
+
+/* nueva palabra */
+
+    const btnNew = document.getElementById('nuevo');
+    btnNew.addEventListener('click', function() {
+    
+    document.getElementById("input_text").value = " ";
+    document.getElementById("texto2").style.display = "none";
+    document.getElementById("copy").style.display = "none";
+    document.getElementById("img").style.display = "show";
+    document.getElementById("img").style.display = "inherit";
+    });
+         
